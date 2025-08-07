9edcbe2ca27614c3295fafcf8925b221d97e258a
gingray <gingray.dev@gmail.com>
2025-08-08 00:46:02 +0200

    add specs

---
+++ spec/fixtures/test_4.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module XXX
+  module YYY
+    class ZZZ
+      def call(a, b)
+        puts a + b
+      end
+    end
+  end
+end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-RSpec.describe RubyRewriter, focus: true do
+RSpec.describe RubyRewriter do
   context 'parse test_2.rb' do
     let(:ruby_code) { read_fixture('test_2.rb') }
     let(:buffer) { [] }
@@ -21,8 +21,43 @@ RSpec.describe RubyRewriter, focus: true do
     end
   end
 
-  context 'construct log' do
-    let(:log) do
+  context 'test_4.rb namespaces' do
+    let(:ruby_code) { read_fixture('test_4.rb') }
+    let(:buffer) { [] }
+    let(:logger) do
+      lambda do |prefix, msg|
+        buffer << [prefix, msg]
+      end
+    end
+    let(:rewriter) { RubyRewriter.new(logger: logger) }
+
+    it do
+      new_code = rewriter.call(ruby_code)
+      expect do
+        eval(new_code)
+        Object.const_get(rewriter.current_class).new.call
+      end.not_to raise_error
+      expect(rewriter.call(ruby_code)).to eq('')
+    end
+  end
+
+  context 'test_5.rb namespaces', focus: true do
+    let(:ruby_code) { read_fixture('test_5.rb') }
+    let(:buffer) { [] }
+    let(:logger) do
+      lambda do |prefix, msg|
+        buffer << [prefix, msg]
+      end
+    end
+    let(:rewriter) { RubyRewriter.new(logger: logger) }
+
+    it do
+      new_code = rewriter.call(ruby_code)
+      expect do
+        eval(new_code)
+        Object.const_get(rewriter.current_class).new.call
+      end.not_to raise_error
+      expect(rewriter.call(ruby_code)).to eq('')
     end
   end
 end
